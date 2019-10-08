65810e8a774dda63e4e978acdbe9b4e7653857e8
LasBoi <Azlj1689gk>
2019-10-08 17:48:30 +0200

    Added testIfConsolePrintsTwo which fails the test

--- test/Opgave_6_6/PatternsTest.java
+++ test/Opgave_6_6/PatternsTest.java
@@ -6,8 +6,12 @@ public class PatternsTest extends TestCase {
 
     /*
         Test Case
+
         Input: n = 1
         Forventet output: 1
+
+        Input: n = 1
+        Forventet output: 2
      */
 
     public void testIfConsolePrintsOne() {
@@ -19,6 +23,15 @@ public class PatternsTest extends TestCase {
         assertEquals(1, result);
 
 
+    }
+
+    public void testIfConsolePrintsTwo() {
+
+        int n = 1;
+
+        int result = Patterns.number(n);
+
+        assertEquals(2, result);
     }
 
 }
